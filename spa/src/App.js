2afa289367326087837b7a0fec2b266bc8789f36
Johannes Ewald <johannes.ewald@peerigon.com>
2017-06-22 11:59:15 +0200

    Add working state

--- spa/src/App.js
+++ spa/src/App.js
@@ -3,16 +3,23 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+    message() {
+        return this.toString();
+    }
     render() {
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
+                    <h2>{this.message()}</h2>
                 </div>
                 <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+                    To get started, edit
+                    {" "}
+                    <code>src/App.js</code>
+                    {" "}
+                    and save to reload.
+                </p>
             </div>
         );
     }
