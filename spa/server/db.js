450db70e8a06aaa116f6358b97f32f65e2e7f6c0
Johannes Ewald <johannes.ewald@peerigon.com>
2017-06-28 15:48:21 +0200

    Simplify server middleware stack

--- spa/server/db.js
+++
@@ -1,3 +0,0 @@
-export default callback => {
-    setImmediate(callback);
-};
--- spa/server/index.js
+++ spa/server/index.js
@@ -5,8 +5,6 @@ import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import helmet from "helmet";
-import initializeDb from "./db";
-import middleware from "./middleware";
 import config from "../config/server";
 import fakeApi from "./api";
 
@@ -27,23 +25,22 @@ app.use(
         limit: config.bodyLimit,
     })
 );
-
-initializeDb(db => {
-    app.use(middleware({ config, db }));
-
-    app.use("/api", fakeApi.getMiddleware());
-    app.use(express.static(path.resolve(__dirname, "..", "public")));
-    app.use((req, res, next) => {
-        res.sendfile(pathToIndexHtml);
-    });
-
-    app.server.listen(
-        process.env.PORT || config.port,
-        config.hostname || "localhost",
-        () => {
-            console.log(`Started on port ${ app.server.address().port }`);
-        }
-    );
+app.use("/api", (req, res, next) => {
+    // Fake a delayed DB response
+    setTimeout(next, config.responseDelay);
 });
+app.use("/api", fakeApi.getMiddleware());
+app.use(express.static(path.resolve(__dirname, "..", "public")));
+app.use((req, res, next) => {
+    res.sendfile(pathToIndexHtml);
+});
+
+app.server.listen(
+    process.env.PORT || config.port,
+    config.hostname || "localhost",
+    () => {
+        console.log(`Started on port ${ app.server.address().port }`);
+    }
+);
 
 export default app;
