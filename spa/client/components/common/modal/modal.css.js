f36c9e42cdc19f3be9ff01215a1c47b76e5d143e
Johannes Ewald <johannes.ewald@peerigon.com>
2017-06-30 11:00:06 +0200

    Add modal backdrop animation

--- spa/client/components/common/modal/modal.css.js
+++ spa/client/components/common/modal/modal.css.js
@@ -1,14 +1,26 @@
 import { css } from "glamor";
 import { modal as modalZIndex } from "../../../styles/zIndex";
+import { msToSeconds } from "../../../styles/timing";
+
+const backdropFadeOutStyles = {
+    backgroundColor: "rgba(0, 0, 0, 0)",
+};
+const backdropFadeInStyles = {
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+};
 
 const fadeIn = css.keyframes({
-    from: {
-        backgroundColor: "rgba(0, 0, 0, 0)",
-    },
-    to: {
-        backgroundColor: "rgba(0, 0, 0, 0.4)",
-    },
+    from: backdropFadeOutStyles,
+    to: backdropFadeInStyles,
 });
+
+const fadeOut = css.keyframes({
+    from: backdropFadeInStyles,
+    to: backdropFadeOutStyles,
+});
+
+export const fadeDuration = 100;
+const fadeDurationCss = msToSeconds(fadeDuration) + "s";
 
 export const root = css({
     position: "absolute",
@@ -23,5 +35,12 @@ export const backdrop = css({
     display: "block",
     width: "100%",
     height: "100%",
-    animation: fadeIn + " 0.3s forwards ease-in-out",
+});
+
+export const backdropFadeIn = css({
+    animation: `${ fadeIn } ${ fadeDurationCss } ease-in-out forwards`,
+});
+
+export const backdropFadeOut = css({
+    animation: `${ fadeOut } ${ fadeDurationCss } ease-in-out forwards`,
 });
--- spa/client/components/common/modal/modal.js
+++ spa/client/components/common/modal/modal.js
@@ -1,20 +1,40 @@
 import { Component, render as preactRender } from "preact";
-import { root, backdrop } from "./modal.css";
+import {
+    root,
+    backdrop,
+    backdropFadeIn,
+    backdropFadeOut,
+    fadeDuration,
+} from "./modal.css";
 import GoBack from "../router/goBack";
 
 export default class Modal extends Component {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
     componentWillMount() {
         this.root = document.createElement("section");
         document.body.appendChild(this.root);
     }
-    componentWillUnmount() {
-        document.body.removeChild(this.root);
+    handleClick(event) {
+        const goBackLink = event.target;
+
+        goBackLink.classList.remove(backdropFadeIn);
+        goBackLink.classList.add(backdropFadeOut);
+
+        setTimeout(() => {
+            document.body.removeChild(this.root);
+        }, fadeDuration);
     }
     render() {
         preactRender(
             <div class={root}>
                 {"..."}
-                <GoBack class={backdrop} />
+                <GoBack
+                    class={[backdrop, backdropFadeIn].join(" ")}
+                    onClick={this.handleClick}
+                />
             </div>,
             this.root
         );
