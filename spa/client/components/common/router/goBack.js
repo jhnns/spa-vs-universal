230d9f282ec2f5969c7c7fff6d6a7d41fd64b33e
Johannes Ewald <johannes.ewald@peerigon.com>
2017-06-30 10:59:58 +0200

    Refactor goBack link component

--- spa/client/components/common/router/goBack.js
+++ spa/client/components/common/router/goBack.js
@@ -1,13 +1,26 @@
 import { Component } from "preact";
 
-function back() {
-    history.back();
-}
-
 export default class GoBack extends Component {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e) {
+        const onClick = this.props.onClick;
+
+        history.back();
+        if (typeof onClick === "function") {
+            onClick(e);
+        }
+    }
     render(props) {
         return (
-            <a {...props} href={"?"} onClick={back}>
+            <a
+                {...props}
+                href={"?"}
+                onClick={this.handleClick}
+                dataNoRouting={true}
+            >
                 {props.children}
             </a>
         );
