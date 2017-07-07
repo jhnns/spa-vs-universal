675140d39c3b485fcbfd46e64fc9d416bfe7171b
Johannes Ewald <johannes.ewald@peerigon.com>
2017-07-07 10:59:58 +0200

    Refactor logo component

--- spa/client/components/header/logo/logo.js
+++ spa/client/components/header/logo/logo.js
@@ -2,18 +2,16 @@ import { Component } from "preact";
 import logoSrc from "../../../assets/img/peerigonLogoMint.svg";
 import { logoImg } from "./logo.css";
 
-export default class HeaderLogo extends Component {
+export default class Logo extends Component {
     render() {
         return (
-            <div>
-                <img
-                    class={logoImg}
-                    src={logoSrc}
-                    /* Do not display when CSS is not working */
-                    height={0}
-                    alt={"Peerigon logo"}
-                />
-            </div>
+            <img
+                class={logoImg}
+                src={logoSrc}
+                /* Do not display when CSS is not working */
+                height={0}
+                alt={"Peerigon logo"}
+            />
         );
     }
 }
--- universal/app/components/header/logo/logo.js
+++ universal/app/components/header/logo/logo.js
@@ -1,19 +1,14 @@
-import { Component } from "preact";
 import logoSrc from "../../../assets/img/peerigonLogoMint.svg";
 import { logoImg } from "./logo.css";
 
-export default class HeaderLogo extends Component {
-    render() {
-        return (
-            <div>
-                <img
-                    class={logoImg}
-                    src={logoSrc}
-                    /* Do not display when CSS is not working */
-                    height={0}
-                    alt={"Peerigon logo"}
-                />
-            </div>
-        );
-    }
+export default function Logo() {
+    return (
+        <img
+            class={logoImg}
+            src={logoSrc}
+            /* Do not display when CSS is not working */
+            height={0}
+            alt={"Peerigon logo"}
+        />
+    );
 }
