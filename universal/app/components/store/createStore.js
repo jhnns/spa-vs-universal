5d2dddcec286a56f15e5a4aa92b9370f49664128
Johannes Ewald <johannes.ewald@peerigon.com>
2017-07-10 21:26:44 +0200

    Add redux devtool support

--- universal/app/components/store/createStore.js
+++ universal/app/components/store/createStore.js
@@ -1,8 +1,16 @@
-import { createStore as reduxCreateStore, compose, applyMiddleware } from "redux";
-import reduxLogger from "redux-logger";
+import { createStore as reduxCreateStore, compose } from "redux";
 import reducer from "./reducer";
 import enhanceStore from "./enhanceStore";
 
 export default function createStore(initialState) {
-    return reduxCreateStore(reducer, initialState, compose(enhanceStore));
+    return reduxCreateStore(
+        reducer,
+        initialState,
+        compose(
+            enhanceStore,
+            // Use redux devtools when installed in the browser
+            // @see https://github.com/zalmoxisus/redux-devtools-extension#implementation
+            typeof devToolsExtension === "undefined" ? f => f : devToolsExtension() // eslint-disable-line no-undef
+        )
+    );
 }
