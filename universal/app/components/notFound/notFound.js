2f78d1ae1568b98aca572b6abcb4a324639f35bf
Johannes Ewald <johannes.ewald@peerigon.com>
2017-07-10 22:17:42 +0200

    Add notFound route

--- universal/app/components/notFound/notFound.js
+++ universal/app/components/notFound/notFound.js
@@ -1,3 +1,25 @@
+import defineState from "../store/defineState";
 import renderChild from "../util/renderChild";
+import { state as documentState } from "../document/document";
+
+export const state = defineState({
+    scope: "notFound",
+    hydrate(dehydratedState) {
+        return {
+            ...dehydratedState,
+        };
+    },
+    actions: {
+        enter: () => (getState, patchState, dispatchAction, execEffect) => {
+            dispatchAction(
+                documentState.actions.update({
+                    statusCode: 404,
+                    title: "Not Found",
+                    headerTags: [],
+                })
+            );
+        },
+    },
+});
 
 export default renderChild;
