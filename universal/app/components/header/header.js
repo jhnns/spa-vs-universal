6151d9c27c49c85567dc2738ce8428a20075f8aa
Johannes Ewald <johannes.ewald@peerigon.com>
2017-07-11 16:21:01 +0200

    Add nav component

--- universal/app/components/header/header.js
+++ universal/app/components/header/header.js
@@ -1,24 +1,27 @@
-import { Component } from "preact";
 import Logo from "./logo/logo";
-// import Nav from "./nav/nav";
+import Nav from "./nav/nav";
 import Link from "../router/link";
 import routes from "../../routes";
 import { root, content, logo, nav, headline, session, offscreen } from "./header.css";
+import defineComponent from "../util/defineComponent";
 // import Session from "./session/session";
 
-export default function Header() {
-    return (
-        <header {...root}>
-            <div {...content}>
-                <Link route={routes.top5} {...logo}>
-                    <Logo />
-                    <h1 {...headline}>
-                        <span {...offscreen}>Peerigon</span> News
-                    </h1>
-                </Link>
-                {/* <Nav class={nav} /> */}
-                {/* <Session class={session} /> */}
-            </div>
-        </header>
-    );
-}
+export default defineComponent({
+    name: "Header",
+    render() {
+        return (
+            <header {...root}>
+                <div {...content}>
+                    <Link route={routes.top5} {...logo}>
+                        <Logo />
+                        <h1 {...headline}>
+                            <span {...offscreen}>Peerigon</span> News
+                        </h1>
+                    </Link>
+                    <Nav styles={nav} />
+                    {/* <Session class={session} /> */}
+                </div>
+            </header>
+        );
+    },
+});
--- universal/app/components/header/nav/nav.js
+++ universal/app/components/header/nav/nav.js
@@ -1,32 +1,33 @@
-import { Component } from "preact";
 import Link from "../../router/link";
 import { list, listItem } from "./nav.css";
 import { link, activeLink } from "../link.css";
 import routes from "../../../routes";
 import { nbsp } from "../../../util/htmlEntities";
+import defineComponent from "../../util/defineComponent";
 
-export default class Nav extends Component {
+export default defineComponent({
+    name: "Nav",
     render(props) {
         return (
-            <nav class={props.class}>
-                <ul class={list}>
-                    <li class={listItem}>
-                        <Link route={routes.top5} class={link} activeClass={activeLink}>
+            <nav {...props.styles}>
+                <ul {...list}>
+                    <li {...listItem}>
+                        <Link route={routes.top5} activeClass={activeLink} {...link}>
                             Top{nbsp}5
                         </Link>
                     </li>
-                    <li class={listItem}>
-                        <Link route={routes.allPosts} class={link} activeClass={activeLink}>
+                    <li {...listItem}>
+                        <Link route={routes.allPosts} activeClass={activeLink} {...link}>
                             All
                         </Link>
                     </li>
-                    <li class={listItem}>
-                        <Link route={routes.about} class={link} activeClass={activeLink}>
+                    <li {...listItem}>
+                        <Link route={routes.about} activeClass={activeLink} {...link}>
                             About
                         </Link>
                     </li>
                 </ul>
             </nav>
         );
-    }
-}
+    },
+});
