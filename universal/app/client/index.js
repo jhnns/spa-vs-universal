2eb44d59ae48cdd1ba0e427b04c9a864e444976c
Johannes Ewald <johannes.ewald@peerigon.com>
2017-07-13 23:23:19 +0200

    Update document title

--- universal/app/client/index.js
+++ universal/app/client/index.js
@@ -6,6 +6,7 @@ function startApp() {
     const createApp = require("../createApp").default;
     const chunkState = require("../components/chunks/chunks").state;
     const routerState = require("../components/router/router").state;
+    const documentState = require("../components/document/document").state;
     const { app, store } = createApp(window.__PRELOADED_STATE__ || {});
 
     store.dispatch(routerState.actions.init(window.location.pathname + window.location.search + window.location.hash));
@@ -15,6 +16,9 @@ function startApp() {
         const applyLazyStylesheets = require("./applyLazyStylesheets").default;
 
         applyLazyStylesheets();
+    });
+    store.watch(documentState.select, newDocumentState => {
+        document.title = newDocumentState.title;
     });
 }
 
