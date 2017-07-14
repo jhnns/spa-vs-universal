96e523cb9f94f8054d8fa08f10c8a759aca31cdc
Johannes Ewald <johannes.ewald@peerigon.com>
2017-07-14 16:14:08 +0200

    Add history synchronization

--- universal/app/client/setupSideEffects.js
+++ universal/app/client/setupSideEffects.js
@@ -1,3 +1,5 @@
+import onLinkClick from "nanohref";
+import onHistoryPop from "nanohistory";
 import { state as documentState } from "../components/document/document";
 import { state as routerState } from "../components/router/router";
 
@@ -6,6 +8,38 @@ export default function setupSideEffects(store) {
         document.title = newDocumentState.title;
     });
     store.watch(routerState.select, (newRouterState, oldRouterState) => {
-        console.log("Router state changed");
+        const newHistory = newRouterState.history.length;
+        const oldHistory = oldRouterState.history.length;
+        const lengthDiff = newHistory.length - oldHistory.length;
+
+        if (lengthDiff === 0) {
+            history.replaceState(null, "", newHistory[newHistory.length - 1]);
+        } else if (lengthDiff > 0) {
+            for (let i = 0; i < lengthDiff; i++) {
+                history.pushState(null, "", newHistory[oldHistory.length + i]);
+            }
+        } else if (lengthDiff < 0) {
+            const lengthDiff = window.history.length - newHistory.length;
+
+            for (let i = 0; i < lengthDiff; i++) {
+                history.back();
+            }
+        }
+    });
+    onHistoryPop(location => {
+        store.dispatch(routerState.actions.pop());
+    });
+    onLinkClick(node => {
+        const url = node.href;
+
+        if (node.hasAttribute("data-route") === false) {
+            window.location = url;
+
+            return;
+        }
+
+        const action = routerState.actions[node.hasAttribute("data-replace-url") ? "replace" : "push"];
+
+        store.dispatch(action(url));
     });
 }
