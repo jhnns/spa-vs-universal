60d09557846fbe92320e3ed5de03f47962f9fb49
Johannes Ewald <johannes.ewald@peerigon.com>
2017-07-15 13:27:06 +0200

    Remove unused effect

--- universal/app/effects/forward/forward.browser.js
+++
@@ -1,8 +0,0 @@
-import { getRouter } from "../initRouter/initRouter.browser";
-
-export default function forward(href, { replaceRoute = href === window.location.href } = {}) {
-    const saveState = replaceRoute === true ? window.history.replaceState : window.history.pushState;
-
-    saveState.call(history, {}, "", href);
-    getRouter()(location.pathname);
-}
--- universal/app/effects/forward/forward.node.js
+++
@@ -1,3 +0,0 @@
-export default function forward(href, { replaceRoute } = {}) {
-    // Cannot be handled as a side-effect in node
-}
