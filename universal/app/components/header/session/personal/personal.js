967e5022dff88858aa0073afb4b2d4af416284df
Johannes Ewald <johannes.ewald@peerigon.com>
2017-07-17 12:11:14 +0200

    Refactor personal component to functional component

--- universal/app/components/header/session/personal/personal.js
+++ universal/app/components/header/session/personal/personal.js
@@ -1,32 +1,26 @@
-import defineComponent from "../../../util/defineComponent";
 import { root, userName, userImage } from "./personal.css";
 import { nbsp } from "../../../../util/htmlEntities";
 import Link from "../../../router/link";
 import { link } from "../../link.css";
 
-const name = "headerSessionPersonal";
+export default function HeaderSessionPersonal(props) {
+    const user = props.user;
 
-export default defineComponent({
-    name,
-    render(props) {
-        const user = props.user;
+    if (user === null) {
+        return null;
+    }
 
-        if (user === null) {
-            return null;
-        }
-
-        return (
-            <div {...root}>
-                <img src={user.image} alt={user.name} {...userImage} />
-                <span {...userName}>
-                    {user.name}
-                </span>
-                <span>
-                    <Link {...link}>
-                        Log{nbsp}out
-                    </Link>
-                </span>
-            </div>
-        );
-    },
-});
+    return (
+        <div {...root}>
+            <img src={user.image} alt={user.name} {...userImage} />
+            <span {...userName}>
+                {user.name}
+            </span>
+            <span>
+                <Link {...link}>
+                    Log{nbsp}out
+                </Link>
+            </span>
+        </div>
+    );
+}
