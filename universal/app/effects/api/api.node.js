b6952bfb46d81cecbb648b69b8d3b98550ee1077
Johannes Ewald <johannes.ewald@peerigon.com>
2017-07-18 17:07:51 +0200

    Implement session forwarding

--- universal/app/effects/api/api.node.js
+++ universal/app/effects/api/api.node.js
@@ -3,6 +3,38 @@ import config from "../../../config/server";
 
 const root = `http://${ config.hostname }:${ config.port }/api`;
 
-export default function api() {
-    return (url, options) => fetch(root + url, options);
+function includeCredentials(options) {
+    return typeof options.credentials === "string" && options.credentials.toLowerCase() === "same-origin";
+}
+
+export default function api({ req, res }) {
+    return (url, options = {}) => {
+        if (includeCredentials(options)) {
+            options.headers = options.headers || {};
+            options.headers.cookie = req.headers.cookie;
+        }
+
+        return fetch(root + url, options).then(apiRes => {
+            const cookie = apiRes.headers.get("set-cookie");
+
+            if (!cookie) {
+                return apiRes;
+            }
+
+            // The API responded with a different session, we need to switch this one
+            return new Promise((resolve, reject) => {
+                req.session.destroy(err => {
+                    if (err) {
+                        reject(err);
+
+                        return;
+                    }
+
+                    res.removeHeader("Set-Cookie");
+                    res.header("Set-Cookie", cookie);
+                    resolve(apiRes);
+                });
+            });
+        });
+    };
 }
