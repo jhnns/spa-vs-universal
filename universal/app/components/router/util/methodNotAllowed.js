27c61ab085e35cb0351899874ebbbdfa2d509782
Johannes Ewald <johannes.ewald@peerigon.com>
2017-08-04 23:50:18 +0200

    Remove unused file

--- universal/app/components/router/util/methodNotAllowed.js
+++
@@ -1,7 +0,0 @@
-export default function methodNotAllowed(allowedMethods, requestPathname) {
-    return {
-        statusCode: 405,
-        title: "Method not allowed",
-        message: `Only ${ allowedMethods.join(", ") } is allowed at ${ requestPathname }.`,
-    };
-}
